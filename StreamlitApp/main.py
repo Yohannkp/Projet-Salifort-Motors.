e7d2389b22a091ee942d646356f57b9e830f7c09
yohannkp <yoharh56@gmail.com>
2025-03-11 13:44:24 +0100

    correction de l'application streamlit

--- StreamlitApp/main.py
+++ StreamlitApp/main.py
@@ -62,7 +62,7 @@ data = {
 
 # Envoi des données à l'API et affichage des résultats
 if submitted:
-    url = "http://127.0.0.1:8000/predict"  # Assurez-vous que FastAPI tourne sur ce port
+    url = "projet-salifort-motors-production.up.railway.app/predict"  # Assurez-vous que FastAPI tourne sur ce port
     response = requests.post(url, json=data)
     
     if response.status_code == 200:
--- main.py
+++ main.py
@@ -60,4 +60,4 @@ def predict_turnover(data: EmployeeData):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))  # Railway définit dynamiquement le port
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="", port=port)
